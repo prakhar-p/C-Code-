4f71890527cde076f952f99b5fafd0ad1b0f3c86
prakhar-p <74608125+prakhar-p@users.noreply.github.com>
2020-12-20 22:07:19 +0530

    Update subSequence.cpp

--- subSequence.cpp
+++ subSequence.cpp
@@ -9,9 +9,6 @@ void printAllSubsequences(string a, string out){
 		return;
 	}
 
-	//string c = "";
-	//for(int j = 1; j < a.length(); j++)
-		//c += a[j];
 	string c=a.substr(1);
 	printAllSubsequences(c, out);
 	printAllSubsequences(c, out + a[0]);
